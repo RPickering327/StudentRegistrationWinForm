8b9f35b7baf80d13319e1ae8159fd3e11515ae75
RPickering327 <rpickering327@Gmail.com>
2018-12-04 21:26:33 +0000

    Unit tests added

---
+++ StudentRegistrationUnitTesting/CourseTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentManagementWF;
+
+namespace StudentRegistrationUnitTesting
+{
+    [TestClass]
+    public class CourseTest
+    {
+        [TestMethod]
+        public void MinimumGradeBelowAndAboveExpected()
+        {
+            //Creat a new instance of course
+            var NewCourse = new Course();
+
+            //Assign a value below 0
+            NewCourse.MinimumGrade = -1;
+
+            //Check the value has not been assigned to minimum grade
+            Assert.AreNotEqual(NewCourse.MinimumGrade, -1);
+
+            //Assign a value above 100
+            NewCourse.MinimumGrade = 101;
+
+            //Check the value has not been assigned to minimum grade
+            Assert.AreNotEqual(NewCourse.MinimumGrade, 101);
+            Assert.AreEqual(NewCourse.Valid, false);
+
+        }
+
+        [TestMethod]
+        public void ValidGradeEntered()
+        {
+            //Creat a new instance of course
+            var NewCourse = new Course();
+
+            //Assign a value below 0
+            NewCourse.MinimumGrade = 55;
+
+            //Check the value has not been assigned to minimum grade
+            Assert.AreEqual(NewCourse.MinimumGrade, 55);
+            Assert.AreEqual(NewCourse.Valid, true);
+        }
+    }
+}
---
+++ StudentRegistrationUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StudentRegistrationUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StudentRegistrationUnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6208d93c-86f0-4272-b9ff-5c7b5f7c46c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
