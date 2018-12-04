447431580c545eb71d3afce05415152814c38859
RPickering327 <rpickering327@Gmail.com>
2018-12-04 21:32:46 +0000

    Unit test update

--- StudentRegistrationUnitTesting/StudentTests.cs
+++ StudentRegistrationUnitTesting/StudentTests.cs
@@ -1,14 +1,43 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentManagementWF;
 
 namespace StudentRegistrationUnitTesting
 {
     [TestClass]
-    public class UnitTest1
+    public class StudentTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InvalidFirstNameAndLastName()
         {
+            //Creat a new instance of course
+            var student = new Student();
+
+            //Assign a value below 0
+            student.StudentFirstName = "";
+            student.StudentLastName = null;
+
+            //Check the value has not been assigned to minimum grade
+            Assert.AreEqual(student.StudentFirstName, null);
+            Assert.AreEqual(student.StudentLastName, null);
+
+
+        }
+
+        [TestMethod]
+        public void ValidFirstNameAndLastName()
+        {
+            //Creat a new instance of course
+            var student = new Student();
+
+            //Assign a value below 0
+            student.StudentFirstName = "John";
+            student.StudentLastName = "Smith";
+
+            //Check the value has not been assigned to minimum grade
+            Assert.AreEqual(student.StudentFirstName, "John");
+            Assert.AreEqual(student.StudentLastName, "Smith");
+
+
         }
     }
 }
